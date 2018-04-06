8b3e8945b5ee4bc0618e92dee5cfe7e01378d377
Arkham <wuxin.zju@qq.com>
2018-04-06 15:52:42 +0800

    minimum changes in space length

--- InsertionSort2.c
+++ InsertionSort2.c
@@ -6,8 +6,8 @@ void InsetionSort2( ElementType A[ ], int N ){
 		low = 1; high = i - 1;
 		while( low<=high ){
 			mid = (low + high) / 2;
-			if( a[mid]>Tmp )	high = mid - 1;
-			else	low = mid + 1;
+			if( a[mid]>Tmp ) high = mid - 1;
+			else low = mid + 1;
 		}
 		for( j=i; j>high+1; j-- )
 			A[j] = A[j-1];
