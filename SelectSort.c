d8bf5ff1675629d79720b5295168d92895a70543
Arkham <wuxin.zju@qq.com>
2018-04-07 17:23:02 +0800

    minimum changes in space length

--- SelectSort.c
+++ SelectSort.c
@@ -3,7 +3,7 @@ void SelectSort( ElementType A[ ], int N ){
 	for( i=0; i<N-1; i++ ){
 		tmp = i;
 		for( j=i+1; j<N; j++ )
-			if( A[j]<A[tmp] )	tmp = j;
-		if( tmp!=i )	swap( &A[tmp], &A[i] );
+			if( A[j]<A[tmp] ) tmp = j;
+		if( tmp!=i ) swap( &A[tmp], &A[i] );
 	}
 }
